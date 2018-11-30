bcccf5c5ba2d6031f2a39a5664b09abea1684cb4
aila ahmed <aila.ahmed02@MacBook-Pro-de-aila.local>
2018-11-30 10:01:07 +0100

    add files gitignore and package and server.js

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const fs = require("fs");
+
+let server = app.listen(8081, function (){
+	let host = server.address().address
+	let port = server.address().port
+	console.log("launch API restful with the following URI http://%s:%s", host, port)
+
+})
